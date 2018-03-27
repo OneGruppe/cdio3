869dd2a5c853d7c9a58a0e3e579c299ef78f2360
Alan <s122417@student.dtu.dk>
2018-03-27 18:34:12 +0200

    edit functionality

--- src/functionality/Functionality.java
+++ src/functionality/Functionality.java
@@ -2,43 +2,37 @@ package functionality;
 
 public class Functionality  implements IFunctionality{
 
-	@Override
 	public void changeUser(int id, String newName, String newPassword, String newIni) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public void createUser(String name, String password, String ini, String cpr) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public void deleteUser(int id) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public UserDTO showUser(int id) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public String showUserAdmin(int id) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public String showUserList() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
+	
 	public String showUserListAdmin() {
 		// TODO Auto-generated method stub
 		return null;
