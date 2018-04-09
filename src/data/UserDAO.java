aca895c94daf837cfb4b239db6164bd7516a0f7c
Burhan27 <32563710+Burhan27@users.noreply.github.com>
2018-04-09 12:41:12 +0200

    i made dao yes

--- src/data/UserDAO.java
+++ src/data/UserDAO.java
@@ -2,13 +2,14 @@ package data;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
-import com.mysql.jdbc.Statement;
-
 public class UserDAO implements IUserDAO {
-	
+
 	Statement stmt = null;
 	Connection connection = null;
 
@@ -16,22 +17,22 @@ public class UserDAO implements IUserDAO {
 	String serverName;
 	String portNumber;
 	String url;
-	String username;
+	String userName;
 	String password;
 
-	public UserDAO(String serverName, String portNumber, String url, String username, String password) {
+	public UserDAO(String serverName, String portNumber, String url, String userName, String password) {
 		this.serverName = serverName;
 		this.portNumber = portNumber;
 		this.url = url;
-		this.username = username;
+		this.userName = userName;
 		this.password = password;		
-		
+
 		try {
 			// Load the JDBC driver
 			Class.forName(driverName);
 
 			// Create a connection to the database
-			connection = DriverManager.getConnection(url, username, password);
+			connection = DriverManager.getConnection(url, userName, password);
 
 		} catch (ClassNotFoundException e) {
 			// Could not find the database driver 
@@ -44,31 +45,92 @@ public class UserDAO implements IUserDAO {
 
 	@Override
 	public UserDTO getUser(int userId) throws DALException {
-		// TODO Auto-generated method stub
-		return null;
+		String query = "SELECT * FROM Users WHERE userID='" + userId + "'"+";";
+
+		try {
+			java.sql.Statement stmt = connection.createStatement();
+			ResultSet rs = stmt.executeQuery(query); 
+			String username = rs.getString("UserName");
+			String ini = rs.getString("ini");
+			String Password = rs.getString("password");
+			String cpr = rs.getString("cpr");
+			UserDTO User = new UserDTO(userId, username, Password, ini, cpr);
+
+			return User;
+
+		} catch (SQLException e) {
+			// Could not connect to the database
+			System.out.println(e.getMessage()); 
+			return null;
+		}
 	}
 
 	@Override
 	public List<UserDTO> getUserList() throws DALException {
-		// TODO Auto-generated method stub
-		return null;
+		List<UserDTO> UserList = new ArrayList<UserDTO>();
+		String query = "SELECT * FROM Users;";
+
+		try {
+			Statement stmt = connection.createStatement();
+			ResultSet rs = stmt.executeQuery(query); 
+			while (rs.next()) {
+				String Username = rs.getString("Username");
+				String Password = rs.getString("password");
+				String ini = rs.getString("ini");
+				String cpr = rs.getString("cpr");
+				int UserId = rs.getInt("userId");
+				UserDTO user = new UserDTO(UserId, Username, Password, ini, cpr);
+				UserList.add(user);
+			}
+			return UserList;
+
+		} catch (SQLException e) {
+			// Could not connect to the database
+			System.out.println(e.getMessage()); 
+			return null;
+		}
 	}
 
 	@Override
 	public void createUser(UserDTO user) throws DALException {
-		// TODO Auto-generated method stub
+		String query = "INSERT INTO Users ( username, password, ini, cpr) VALUES(" + "'" + user.getUserName() + "'" + "'" + user.getPassword()+ "'" + "'" + user.getIni() + "'" + "'" + user.getCpr() + "'" + ");";
+
+		try {
+			Statement stmt = connection.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+
+		} catch (SQLException e) {
+			// Could not connect to the database
+			System.out.println(e.getMessage()); 
+		}
 
 	}
 
 	@Override
 	public void updateUser(UserDTO user) throws DALException {
-		// TODO Auto-generated method stub
+		String query = "UPDATE users SET username =" +"'"+ user.getUserName() +"'"+ ", password =" +"'"+ user.getPassword() +"'"+ ", ini =" +"'"+ user.getIni() +"'"+ ",cpr =" +"'"+ user.getCpr() +"'"+ " WHERE id=" + user.getUserId() + ";";
+		try {
+			Statement stmt = connection.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
 
+		} catch (SQLException e) {
+			// Could not connect to the database
+			System.out.println(e.getMessage()); 
+		}
 	}
 
 	@Override
 	public void deleteUser(int userId) throws DALException {
-		// TODO Auto-generated method stub
+		String query = "DELETE FROM users where userId =" +userId+";";
+
+		try {
+			Statement stmt = connection.createStatement();
+			ResultSet rs = stmt.executeQuery(query); 
+
+		} catch (SQLException e) {
+			// Could not connect to the database
+			System.out.println(e.getMessage()); 
+		}
 
 	}
 
