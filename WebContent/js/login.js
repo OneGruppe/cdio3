8b3a8e3c6df280522c370554993720b46f065b7f
Mathias <mathias_1337@hotmail.com>
2018-04-09 16:30:50 +0200

    Show/hide samt js

--- WebContent/js/login.js
+++ WebContent/js/login.js
@@ -1,16 +1,30 @@
 $(document).ready(function(){
-    $("#slet bruger").click(function(){
-
+    $("#createUserContainer").hide();
+    $("#createUser").click(function(){
+        $("#frontPage").hide(500);
+        $("#createUserContainer").show(500);
     })
-    $("Opret bruger").click(function(){
-        newUser(username, password, ini, CPR, rolle);
+
+    $("#showAllUsers").click(function()
+    {
+         alert("Test virker");
+    })
+    
+    $("#showUser").click(function() 
+    {
+        alert("Test virker");		        		
+    })
+    
+    $("#logout").click(function() 
+    {
+        alert("Test virker");		        		
     })
 });
 
-function newUser(username, password, ini, CPR, rolle) {
+function newUser(username, password, ini, CPR, role) {
     this.username = username;
     this.password = password;
     this.ini = ini;
     this.CPR = CPR;
-    this.rolle = rolle;
+    this.role = role;
 }
