154af38e7d356956b02ca53188b58de04b5a8016
Niklaes <s170423@student.dtu.dk>
2018-04-16 12:28:07 +0200

    JavaScript
    JS: Show/hide fikset

--- WebContent/js/login.js
+++ WebContent/js/login.js
@@ -1,38 +1,27 @@
 $(document).ready(function() {
+
     $("#createUserContainer").hide();
     $("#showAllUsersContainer").hide();
     $("#showUserContainer").hide();
 
-    $("#frontPage").click(function() {
-        $("#createUserContainer").hide(500);
-        $("#showAllUsersContainer").hide(500);
-        $("#ShowUserContainer").hide(500);
-    })
-
     $("#createUser").click(function() {
-        $("#frontPage").hide(500);
-        $("#showAllUsersContainer").hide(500);
-        $("#showUserContainer").hide(500);
-        $("#createUserContainer").show(500);
+        $(".frontPage").slideUp(400);
+        $("#createUserContainer").slideDown(400);
     })
 
     $("#showAllUsers").click(function() {
-         $("frontPage").hide(500);
-         $("showUserContainer").hide(500);
-         $("createUserContainer").hide(500);
-         $("showAllUsersContainer").show(500);
+         $(".frontPage").slideUp(400);
+         $("showAllUsersContainer").slideDown(400);
 
     })
     
     $("#showUser").click(function() {
-        $("frontPage").hide(500);
-        $("createUserContainer").hide(500);
-        $("showAllUsersContainer").hide(500);
-        $("showUserContainer").show(500);
+        $(".frontPage").slideUp(400);
+        $("showUserContainer").slideDown(400);
     })
     
     $("#logout").click(function() {
-        alert("Test virker");		        		
+        alert("Tilbage til login side");		        		
     })
 });
 
